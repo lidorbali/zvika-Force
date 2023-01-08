ea120e4f0bfc1c01d2cd97e9c257e2b8e735e53a
lidorbali <99362432+lidorbali@users.noreply.github.com>
2023-01-08 15:54:30 +0200

    'FixSomeFilesMame'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
-import ShipingScreen from "./screens/ShipingScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -19,7 +19,7 @@ function App() {
             <Route path="/" exact element={<HomeScreen />} />
             <Route path="/login" exact element={<LoginScreen  />} />
             <Route path="/product/:id" element={<ProductScreen />} />
-            <Route path="/shipping" exact element={<ShipingScreen  />} />
+            <Route path="/shipping" exact element={<ShippingScreen  />} />
             <Route path='/cart/:id?' element={<CartScreen />} />
           </Routes>
         </Container>
