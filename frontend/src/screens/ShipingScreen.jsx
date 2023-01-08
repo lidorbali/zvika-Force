c222b9b7e2117c86b166a6746dbadf83f31d1fae
lidorbali <99362432+lidorbali@users.noreply.github.com>
2023-01-08 15:47:57 +0200

    'checkOutStepsValidetion'

--- frontend/src/screens/ShipingScreen.jsx
+++ frontend/src/screens/ShipingScreen.jsx
@@ -3,6 +3,8 @@ import {  Button,   Form} from "react-bootstrap";
 import {useDispatch, useSelector} from 'react-redux'
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import FormContainer from '../components/FormContainer'
+import CheckOutSteps from '../components/CheckOutSteps'
+
 import {saveShippingAddress} from '../actions/cartActions'
 
 const ShippingScreen = () => {
@@ -26,6 +28,7 @@ const ShippingScreen = () => {
  
     return (
     <FormContainer>
+        <CheckOutSteps step1 step2 />
         <h1>Shipping</h1>
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='address'>
